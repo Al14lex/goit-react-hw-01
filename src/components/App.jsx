77dbb64cba54bbb1ed57ccf0cc56e092bb684608
Al14lex <alenahomenko1421@gmail.com>
2024-04-24 16:19:38 +0200

    3

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+ export default function App() {
+  return (
+    <div>
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium recusandae consequuntur porro repudiandae incidunt sunt, ratione minus voluptas autem perspiciatis. Dignissimos repellat quasi quo blanditiis aliquid eveniet nam ab.</p>
+    </div>
+  )
+}
