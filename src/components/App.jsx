fb3217bed6e47c66f85ca947f72e0c2b4eb152e6
Al14lex <alenahomenko1421@gmail.com>
2024-04-26 15:53:02 +0200

    profile done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import userData from "../userData.json";
-import Profile from "./Profile";
+import userData from '../userData.json';
+import Profile from "./Profile/Profile";
+
 
 export default function App() {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,27 @@
+import css from './Profile.module.css';
 
-export default function Profile({ name, tag, location, image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png", stats}) {
-    const { followers, views, likes } = stats;
+export default function Profile({ name, tag, location, image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png", stats }) {
+    const { followers, views, likes } = stats || {};
     return (
-        <div>
-            <div>
+            <div className={css.mainContainer}>
+            <div className={css.container}>
                 <img src={image} alt="User avatar"/>
-                <p>{name}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
+                <p className={css.user}>{name}</p>
+                <p className={css.text}>@{tag}</p>
+                <p className={css.text}>{location}</p>
             </div>
-  
             <ul>
                 <li key="followers">
                     <span>Followers</span>
-                    <span>{followers}</span>
+                    <span><strong>{followers}</strong></span>
                 </li>
                 <li key="views">
                     <span>Views</span>
-                    <span>{views}</span>
+                    <span><strong>{views}</strong></span>
                 </li>
                 <li key="likes">
                     <span>Likes</span>
-                    <span>{likes}</span>
+                    <span><strong>{likes}</strong></span>
                 </li>
             </ul>
         </div>
